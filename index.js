09fc6c2aa955c5154521924b74c6cce2f49a58af
El Ochi <dantezell78@gmail.com>
2016-09-07 12:09:19 -0400

    modified index.js

--- index.js
+++ index.js
@@ -7,9 +7,11 @@ fs.readFile('./example.csv', (err, data) => {
     if (err) throw err;
 
     csv.parse(data, (err, dataParsed) => {
-
-        console.log(dataParsed);
-        updateClient(dataParsed);
+        // testing
+        // console.log(dataParsed);
+        // updateClient(dataParsed);
+        deleteClient(dataParsed);
+        // showCount(dataParsed);
     })
 });
 
@@ -28,15 +30,18 @@ function addClient() {
 };
 
 function updateClient(currentData) {
-    console.log("updating client ");
+    console.log("Updating Client: ");
     var currentClient = prompt("Email: ");
     var found = false;
+    var update = []
+
     for (var i = 1; i < currentData.length; i++) {
         // currentData[i][3];
         if (currentData[i][3] === currentClient) {
             found = true;
+
             console.log("Enter new data");
-          var name = prompt()
+            var name = prompt()
         }
     }
     // if(found === true){
@@ -49,6 +54,28 @@ function updateClient(currentData) {
     }
 };
 
-function deleteClient(currentData) {
+// console.log(prompt('test', 'default'));
 
+function deleteClient(currentData) {
+    console.log("Delete Client ");
+    var currentClient = prompt("Email: ");
+    var found = false;
+    for (var i = 1; i < currentData.length; i++) {
+        if (currentData[i][3] === currentClient) {
+            found = true;
+            console.log("Client Delete");
+            return currentData.splice(i, 1);
+
+        }
+    }
+    if (!found) {
+        console.log("data not found " + currentClient)
+        return currentData;
+
+    }
+}
+
+function showCount(data) {
+    console.log(data.length - 1);
+    return data.length - 1;
 }
